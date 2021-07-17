22484682199128a34730f2f1c1fc047b8513f28b
Jun <hawon.jungo@gmail.com>
2021-07-18 06:28:12 +1000

    add Sign up UI

--- app/src/main/java/com/example/goroster/Login.java
+++ app/src/main/java/com/example/goroster/Login.java
@@ -2,16 +2,37 @@ package com.example.goroster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Pair;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
+
+    Button signUp;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_main);
         setContentView(R.layout.activity_login);
+
+        signUp = findViewById(R.id.btnSignUp);
+
+
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, SignUp.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
