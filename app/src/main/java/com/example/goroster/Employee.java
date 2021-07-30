15ded9e46160b06c8ab1f4103c0926fe6a649ae2
Jun <hawon.jungo@gmail.com>
2021-07-30 18:26:11 +1000

    finish the UI employee activity

--- app/src/main/java/com/example/goroster/Employee.java
+++ app/src/main/java/com/example/goroster/Employee.java
@@ -8,30 +8,39 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class Employee extends AppCompatActivity implements AdapterView.OnItemClickListener {
-    Spinner spinner;
+//implements AdapterView.OnItemClickListener
+public class Employee extends AppCompatActivity  {
+    Spinner spinnerMon,spinnerTue,spinnerWed,spinnerThu,spinnerFri,spinnerSat, spinnerSun;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employee);
 
-        spinner = findViewById(R.id.spinnerMonday);
+        spinnerMon = findViewById(R.id.spinnerMonday);
+        spinnerTue = findViewById(R.id.spinnerTuesday);
+        spinnerWed = findViewById(R.id.spinnerWednesday);
+        spinnerThu = findViewById(R.id.spinnerThursday);
+        spinnerFri = findViewById(R.id.spinnerFriday);
+        spinnerSat = findViewById(R.id.spinnerSaturday);
+        spinnerSun = findViewById(R.id.spinnerSunday);
 
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(Employee.this,R.array.available,android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-       // spinner.setOnItemClickListener(this);
+        spinnerMon.setAdapter(adapter);
+        spinnerTue.setAdapter(adapter);
+        spinnerWed.setAdapter(adapter);
+        spinnerThu.setAdapter(adapter);
+        spinnerFri.setAdapter(adapter);
+        spinnerSat.setAdapter(adapter);
+        spinnerSun.setAdapter(adapter);
+        //spinner.setOnItemClickListener(this);
     }
 
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        String text = parent.getItemAtPosition(position).toString();
-        Toast.makeText(parent.getContext(),text, Toast.LENGTH_SHORT).show();
-    }
+//    @Override
+//    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//        String text = parent.getItemAtPosition(position).toString();
+//        Toast.makeText(parent.getContext(),text, Toast.LENGTH_SHORT).show();
+//    }
 }
