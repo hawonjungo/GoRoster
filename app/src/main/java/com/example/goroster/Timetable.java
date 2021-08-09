785602f6343318820bbb0432f7a783194de0215f
Jun <hawon.jungo@gmail.com>
2021-08-09 13:30:39 +1000

    fix UI

--- app/src/main/java/com/example/goroster/Timetable.java
+++ app/src/main/java/com/example/goroster/Timetable.java
@@ -54,8 +54,10 @@ public class Timetable extends AppCompatActivity {
 
     private List<TestRecycleViewTable> getList() {
         List<TestRecycleViewTable> table = new ArrayList<>();
-        table.add(new TestRecycleViewTable("1","Jun","N/A","7AM-12AM","12AM-17PM","N/A","7AM-17PM","N/A","N/A"));
-        table.add(new TestRecycleViewTable("2","KAS","N/A","N/A","N/A","N/A","12AM-17PM","N/A","N/A"));
+        // add some example for prototype and test table
+        table.add(new TestRecycleViewTable("1","Jun","N/A","7-12 AM","12-17 PM","N/A","7-17 PM","N/A","N/A"));
+        table.add(new TestRecycleViewTable("2","KAS","N/A","N/A","N/A","N/A","12-17 PM","N/A","N/A"));
+        table.add(new TestRecycleViewTable("3","Brash","N/A","7-12 AM","12-17 PM","N/A","7-17 PM","N/A","N/A"));
         return table;
 
     }
